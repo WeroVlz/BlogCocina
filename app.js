a647d2735c5dd615d1953915ca2705fa2daf9f23
Edgar <edgarvlzm26@gmail.com>
2021-10-24 21:57:26 -0500

    cambios

--- app.js
+++ app.js
@@ -3,13 +3,27 @@ const express = require("express");
 const app = express();
 
 app.use("/public", express.static(__dirname + '/public'));
-app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname);
 
 app.get('/', (req,res) => {
-    res.sendFile(__dirname + "/main.html");
+    res.sendFile(__dirname + "/public/files/main.html");
 });
+
+app.get("/aboutus", (req, res) => {
+    res.redirect("/");
+});
+
+app.get("/recipes", (req, res) => {
+    res.redirect("/");
+});
+
 
 
 app.listen(3000, () => {
     console.log("Server listening in port 3000...");
 });
+
