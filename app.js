3da81e922d8e9daeea44c651a4c0b051d686ce08
WeroVlz <90603328+WeroVlz@users.noreply.github.com>
2021-10-26 20:17:25 -0500

    update endpoint

--- app.js
+++ app.js
@@ -14,6 +14,28 @@ app.get('/', (req,res) => {
     res.sendFile(__dirname + "/public/files/main.html");
 });
 
+app.post('/', (req,res) => {
+    const rName = req.body.rName;
+    const qnpName = req.body.qnpName;
+    const ingName = req.body.ingName;
+    const preName = req.body.preName;
+    const imgFile = req.body.imgFile;
+
+
+    const data = {
+        recipeName: rName,
+        specifics: qnpName,
+        ingredients: ingName,
+        preparation: preName,
+        image_url: imgFile
+    }
+
+    
+    res.render(__dirname + "/public/files/card.html", {recipeName:data.recipeName, qnpInfo: data.specifics, ingredients: data.ingredients, preparation: data.preparation});
+
+
+});
+
 app.get("/aboutus", (req, res) => {
     res.redirect("/");
 });
